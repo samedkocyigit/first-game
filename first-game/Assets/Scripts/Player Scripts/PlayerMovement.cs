5a2fa9fdd8aaa9dd4f985b09a6c3938018e2c19e
samedkocyigit <abdulsamed.kcygt@gmail.com>
2023-03-18 16:38:46 +0300

    Created script file for player movement and animation

---
+++ first-game/Assets/Scripts/Player Scripts/PlayerMovement.cs
@@ -0,0 +1,57 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    [SerializeField]
+    private float moveSpeed = 3.5f;
+
+    [SerializeField]
+    private float minBound_X = -71f, maxBound_X = 71f, minBound_Y = -3.3f, maxBound_Y = 0f;
+
+    private Vector3 tempPos;
+
+    private float xAxis, yAxis;
+
+    private PlayerAnimation playerAnimation;
+
+    private void Awake ()
+    {
+        playerAnimation = GetComponent<PlayerAnimation>();
+    }
+
+    // Update is called once per frame
+    void Update ()
+    {
+        HandleMovement();
+        HandleAnimation();
+    }
+    void HandleMovement()
+    {
+        xAxis =Input.GetAxisRaw(TagManger.HORIZONTAL_AXIS);
+        yAxis = Input.GetAxisRaw(TagManger.VERTICAL_AXIS);
+
+        tempPos =transform.position;
+
+        tempPos.x += xAxis * moveSpeed * Time.deltaTime;
+        tempPos.y += yAxis * moveSpeed * Time.deltaTime;
+        if(tempPos.x<minBound_X)
+            tempPos.x = minBound_X;
+        if(tempPos.x>maxBound_X)
+            tempPos.x = maxBound_X;
+        if(tempPos.y<minBound_Y)   
+            tempPos.y = minBound_Y;
+        if(tempPos.y>maxBound_Y)    
+            tempPos.y = maxBound_Y;
+
+        transform.position = tempPos;
+    }
+    void HandleAnimation()
+    {
+        if (Mathf.Abs(xAxis) > 0 || Mathf.Abs(yAxis) > 0)
+            playerAnimation.PlayAnimation(TagManger.WALK_ANIMATION_NAME);
+        else
+            playerAnimation.PlayAnimation(TagManger.IDLE_ANIMATION_NAME);
+    }
+}
